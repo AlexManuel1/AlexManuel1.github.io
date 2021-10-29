d746b8d12db1198952f5e7841b50653633f70900
AlexManuel1 <AlexMatamoros10@gmail.com>
2021-10-29 14:18:54 -0700

    fixed scroll fade in/out

--- index.js
+++ index.js
@@ -28,6 +28,8 @@ $(document).ready(function(){
 });
 
 $(window).scroll(function() {
+    progressBar();
+
     if ($(this).scrollTop()>300)
     {
         $('.bounce').hide(1000);
@@ -38,34 +40,36 @@ $(window).scroll(function() {
     }
 
     // fade in effect
-    $(window).on("load",function() {
-        function fade(pageLoad) {
-          var windowTop=$(window).scrollTop(), windowBottom=windowTop+$(window).innerHeight();
-          var min=0.3, max=1, threshold=0.01;
-          
-          $(".project").each(function() {
-            /* Check the location of each desired element */
-            var objectHeight=$(this).outerHeight(), objectTop=$(this).offset().top, objectBottom=$(this).offset().top+objectHeight;
-            
-            /* Fade element in/out based on its visible percentage */
-            if (objectTop < windowTop) {
-              if (objectBottom > windowTop) {$(this).fadeTo(0,min+((max-min)*((objectBottom-windowTop)/objectHeight)));}
-              else if ($(this).css("opacity")>=min+threshold || pageLoad) {$(this).fadeTo(0,min);}
-            } else if (objectBottom > windowBottom) {
-              if (objectTop < windowBottom) {$(this).fadeTo(0,min+((max-min)*((windowBottom-objectTop)/objectHeight)));}
-              else if ($(this).css("opacity")>=min+threshold || pageLoad) {$(this).fadeTo(0,min);}
-            } else if ($(this).css("opacity")<=max-threshold || pageLoad) {$(this).fadeTo(0,max);}
-          });
-        } fade(true); //fade elements on page-load
-        $(window).scroll(function(){fade(false);}); //fade elements on scroll
-    });
+});
+
+$(window).on("load",function() {
+    function fade(pageLoad) {
+      var windowTop=$(window).scrollTop(), windowBottom=windowTop+$(window).innerHeight();
+      var min=0.3, max=1, threshold=0.01;
+      
+      $(".project").each(function() {
+        /* Check the location of each desired element */
+        var objectHeight=$(this).outerHeight(), objectTop=$(this).offset().top, objectBottom=$(this).offset().top+objectHeight;
+        
+        /* Fade element in/out based on its visible percentage */
+        if (objectTop < windowTop) {
+          if (objectBottom > windowTop) {$(this).fadeTo(0,min+((max-min)*((objectBottom-windowTop)/objectHeight)));}
+          else if ($(this).css("opacity")>=min+threshold || pageLoad) {$(this).fadeTo(0,min);}
+        } else if (objectBottom > windowBottom) {
+          if (objectTop < windowBottom) {$(this).fadeTo(0,min+((max-min)*((windowBottom-objectTop)/objectHeight)));}
+          else if ($(this).css("opacity")>=min+threshold || pageLoad) {$(this).fadeTo(0,min);}
+        } else if ($(this).css("opacity")<=max-threshold || pageLoad) {$(this).fadeTo(0,max);}
+      });
+    } 
+    fade(true); //fade elements on page-load
+    $(window).scroll(function(){fade(false);}); //fade elements on scroll
 });
 
 // progress bar
 // When the user scrolls the page, execute myFunction
-window.onscroll = function() {myFunction()};
+//window.onscroll = function() {myFunction()};
 
-function myFunction() {
+function progressBar() {
   var winScroll = document.body.scrollTop || document.documentElement.scrollTop;
   var height = document.documentElement.scrollHeight - document.documentElement.clientHeight;
   var scrolled = (winScroll / height) * 100;
